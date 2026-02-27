d55a631571fabda8081ca47ac0b31c4b2c8cddb8
marek <m@arek.sk>
2026-02-27 11:54:10 +0100

    oprava sqlite query

--- utils/meteostanice.js
+++ utils/meteostanice.js
@@ -118,7 +118,7 @@ export default class Meteostanice {
     static editOwnerOnOwned(owner, newOwner) {
         meteostaniceDB.prepare(`
             update list
-            owner = ?
+            set owner = ?
             where owner = ?;
         `).run(newOwner, owner)
     }
